d55c82024f5cc4248f1baec44b771f4332ace771
Tony <t4tonykuriakose@gmail.com>
2025-01-07 16:52:15 +0530

    signup login

--- server/server.js
+++ server/server.js
@@ -10,14 +10,12 @@ dotenv.config()
 const userRouter=require('./routers/userRouter')
 const adminRouter =require('./routers/adminRouter')
 
-const PORT=process.env.PORT || 5000
-
 app.use(express.static('public'));
 app.use(express.json())
 app.use(cookieParser())
 
 
-//cors
+
 app.use(cors({
     origin:["http://localhost:5173"],
     credentials:true
@@ -29,6 +27,8 @@ app.use('/profileimages',express.static('public'))
 app.use('/',userRouter)
 app.use('/admin',adminRouter)
 
+
+const PORT=process.env.PORT || 5000
 app.listen(PORT,(error)=>{
     if (error) {
         console.error('Error starting the server:', error);
